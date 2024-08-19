ab98f29b3c6c7fb7633655332e843ea5aac66bcc
Ghuni <info@mngunimabaleha.co.za>
2024-08-19 17:33:50 +0200

    front-end from mister maputla

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+//screens...............................................
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' Component={HomeScreen} exact />
+            <Route path='/login' Component={LoginScreen} />
+            <Route path='/register' Component={RegisterScreen} />
+            <Route path='/profile' Component={ProfileScreen} />
+            <Route path='/shipping' Component={ShippingScreen} />
+            <Route path='/payment' Component={PaymentScreen} />
+            <Route path='/placeorder' Component={PlaceOrderScreen} />
+            <Route path='/product/:id' Component={ProductScreen}></Route>
+            <Route path='/cart/:id?' Component={CartScreen}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
